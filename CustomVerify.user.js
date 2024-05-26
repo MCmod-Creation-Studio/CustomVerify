96aaa98617326a581068bae3d3ec3f53869bc3f6
寒冽 <48323966+HanlieChina@users.noreply.github.com>
2024-05-26 18:52:52 +0800

    Update CustomVerify.user.js
    v0.4

--- CustomVerify.user.js
+++ CustomVerify.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         CustomVerify
 // @namespace    mcmod
-// @version      0.3
+// @version      0.4
 // @description  Custom verify content in editor before submitting
 // @author       寒冽
 // @match        https://www.mcmod.cn/*
@@ -20,7 +20,13 @@
 
         const result = originalGetDataVerify(t);
 
+        // main function
         function Verify(condition, level, message) {
+            if (typeof condition !== 'boolean') {
+                console.log('Invalid condition.');
+                return;
+            }
+
             if (condition) {
                 switch (level) {
                     case "error":
@@ -33,58 +39,74 @@
                         result.info[Object.keys(result.info).length] = message;
                         break;
                     default:
-                        result.warning[Object.keys(result.warning).length] = message;
+                        result.info[Object.keys(result.info).length] = message;
                 }
             }
         }
 
         // Example
-        const content = removeHtmlTag(editor.getContent()).trim();
+        Verify(isenameChineseInput(), "Warning", "次要名称含中文。");
+        Verify(containsBilibiliSpmId(), "Warning", "相关链接含追踪。");
+        Verify(!CurseForgeProjectID(), "error", "CurseForge Project ID格式错误。");
+        Verify(isCategory15Selected(), "info", "是国创整合包。");
+        Verify(isTooShort(), "warning", "正文过短。");
 
-        // Info
-        const customErrors = {
-            "exampleError1": "Error1",
-            "exampleError2": "Error2"
-        };
-
-        const customWarnings = {
-            "exampleWarning1": "Warning1",
-            "exampleWarning2": "Warning2"
-        };
-
-        const customInfos = {
-            "exampleInfo1": "Info1",
-            "exampleInfo2": "Info2"
-        };
-        
-        /*
-        if (content.includes("测试")) {
-            result.error[Object.keys(result.error).length] = customErrors.exampleError1;
-        }
-
-        if (content.length > 100) {
-            result.warning[Object.keys(result.warning).length] = customWarnings.exampleWarning1;
-        }
-
-        const keywords = ["测试1", "测试2", "背包"];
-        if (keywords.some(keyword => content.includes(keyword))) {
-            result.warning[Object.keys(result.warning).length] = "含自定义用语";
-        }
-
-        if (content.length > 200) {
-            result.info[Object.keys(result.info).length] = customInfos.exampleInfo1;
-        }
-        */
-
-        /*
-        Verify(content.includes("测试"), "error", customErrors.exampleError1);
-        Verify(content.length > 100, "warning", customWarnings.exampleWarning1);
-        const keywords = ["测试1", "测试2", "背包"];
-        Verify(keywords.some(keyword => content.includes(keyword)), "warning", "含自定义用语");
-        Verify(content.length > 200, "info", customInfos.exampleInfo1);
-        */
-        
         return result;
     };
+
+
+    function isenameChineseInput() {
+        // 获取 次要名称 内容
+        const input = document.querySelector('input[data-multi-id="ename"]').value;
+
+        // 中文字符
+        const chineseRegex = /[\u4e00-\u9fa5]/;
+        return chineseRegex.test(input);
+    }
+
+    function CurseForgeProjectID() {
+        const input = document.querySelector('input[data-multi-id="cfprojectid"]').value;
+
+        // 数字（组）或空
+        const numericRegex = /^(\d+(,\d+)*)?$/;
+        return numericRegex.test(input);
+    }
+
+    function containsBilibiliSpmId() {
+        // 获取 class="link-text-href" 的 input 元素
+        const inputElement = document.querySelector('input.link-text-href');
+
+        // 不存在直接返回 false
+        if (!inputElement) {
+            return false;
+        }
+
+        // 获取输入的值
+        const inputValue = inputElement.value;
+
+        // 检查输入的值是否包含 "/?spm_id_from="
+        return inputValue.includes("/?spm_id_from=");
+    }
+
+    function isCategory15Selected() {
+        // 标签为国创(id = 15)的整合包
+        const element = document.querySelector('a[data-multi-name="modpack-data"][data-multi-id="category"][data-category-id="15"]');
+
+        // 检查元素是否为true
+        if (element && element.getAttribute('data-category-selected') === "true") {
+            return true;
+        }
+
+        return false;
+    }
+
+    // 正文
+    function isTooShort() {
+        const content = removeHtmlTag(editor.getContent()).trim();
+        if (content.length < 10) {
+            return true;
+        }
+        return false;
+    }
 
 })();
